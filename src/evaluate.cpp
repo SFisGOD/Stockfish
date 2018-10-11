c4633675ce4ff57ca275fae11607f64aa6d56251
SFisGOD <jonathandumale@gmail.com>
2018-10-11 18:03:07 +0800

    4498219

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -170,7 +170,7 @@ namespace {
   constexpr Score ThreatByKing       = S( 23, 76);
   constexpr Score ThreatByPawnPush   = S( 45, 40);
   constexpr Score ThreatByRank       = S( 16,  3);
-  constexpr Score ThreatBySafePawn   = S(173,102);
+  constexpr Score ThreatBySafePawn   = S(183,112);
   constexpr Score TrappedRook        = S( 92,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 29);
