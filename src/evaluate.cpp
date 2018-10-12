b30a4c56b2b7ecbbda498527626f033eee81a1ac
SFisGOD <jonathandumale@gmail.com>
2018-10-12 21:28:29 +0800

    4532604

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -160,7 +160,7 @@ namespace {
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 57, 32);
   constexpr Score KingProtector      = S(  6,  6);
-  constexpr Score KnightOnQueen      = S( 21, 11);
+  constexpr Score KnightOnQueen      = S( 26, 13);
   constexpr Score LongDiagonalBishop = S( 46,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 13,  6);
