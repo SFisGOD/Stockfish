b423127a8db0bc9af51cf25c71d712c258f4b12d
SFisGOD <jonathandumale@gmail.com>
2018-10-21 14:26:25 +0800

    4273853

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -165,7 +165,7 @@ namespace {
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 13,  6);
   constexpr Score PawnlessFlank      = S( 19, 84);
-  constexpr Score QueenWithLessEnemy = S(  4,  2);
+  constexpr Score QueenWithLessPieces= S(  2,  1);
   constexpr Score RookOnPawn         = S( 10, 30);
   constexpr Score SliderOnQueen      = S( 42, 21);
   constexpr Score ThreatByKing       = S( 23, 76);
@@ -604,7 +604,7 @@ namespace {
 	
 	if (pos.count<QUEEN>(Us) == 1 && pos.count<QUEEN>(Them) == 0)
 	{
-		score += QueenWithLessEnemy * (15 - pos.count<ALL_PIECES>(Them));
+		score += QueenWithLessPieces * (31 - pos.count<ALL_PIECES>());
 	}
 
     if (T)
