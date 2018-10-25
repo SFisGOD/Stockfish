e77649d50ba23fb3bc35640bb4f95ce9b3cbda7b
SFisGOD <jonathandumale@gmail.com>
2018-10-25 08:35:56 +0800

    4698770

--- src/types.h
+++ src/types.h
@@ -180,7 +180,7 @@ enum Value : int {
   KnightValueMg = 784,   KnightValueEg = 868,
   BishopValueMg = 828,   BishopValueEg = 916,
   RookValueMg   = 1286,  RookValueEg   = 1378,
-  QueenValueMg  = 2550,  QueenValueEg  = 2720,
+  QueenValueMg  = 2517,  QueenValueEg  = 2720,
 
   MidgameLimit  = 15258, EndgameLimit  = 3915
 };
