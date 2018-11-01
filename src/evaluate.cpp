944dd3eedb40e3fbc6726e9f78abfff1b4240f85
SFisGOD <jonathandumale@gmail.com>
2018-11-01 23:04:04 +0800

    3556672

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -127,7 +127,9 @@ namespace {
 
   // RookOnFile[semiopen/open] contains bonuses for each rook when there is
   // no (friendly) pawn on the rook file.
-  constexpr Score RookOnFile[] = { S(20, 7), S(45, 20) };
+  Score RookOnFile[] = { S(20, 7), S(45, 20) };
+  
+  TUNE(RookOnFile);
 
   // ThreatByMinor/ByRook[attacked PieceType] contains bonuses according to
   // which piece type attacks which one. Attacks on lesser pieces which are
