7687342196d638e6121f3d38c064fe0065a8b5a6
SFisGOD <jonathandumale@gmail.com>
2018-11-05 14:23:44 +0800

    Tweak LDB. Bench: 3880838

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -161,7 +161,7 @@ namespace {
   constexpr Score Hanging            = S( 57, 32);
   constexpr Score KingProtector      = S(  6,  6);
   constexpr Score KnightOnQueen      = S( 21, 11);
-  constexpr Score LongDiagonalBishop = S( 46,  0);
+  constexpr Score LongDiagonalBishop = S( 46, 12);
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 13,  6);
   constexpr Score PawnlessFlank      = S( 19, 84);
