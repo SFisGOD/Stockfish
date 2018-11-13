ff5d82bd5d1bd57b884572730f19ad2e526bb866
SFisGOD <jonathandumale@gmail.com>
2018-11-13 14:23:52 +0800

    pureStaticEval greater than zero instead of one. Bench: 3690707

--- src/search.cpp
+++ src/search.cpp
@@ -953,7 +953,7 @@ moves_loop: // When in check, search starts from here
 
       // Extension for piece exchange in opposite-colored bishops
       if ( pos.opposite_bishops()
-          && pureStaticEval > 1     
+          && pureStaticEval > 0     
           && type_of(capturedPiece) == type_of(movedPiece))
           extension = ONE_PLY;
 		  
