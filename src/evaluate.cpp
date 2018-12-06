d9835115b5a53cafd44a2ab35a3f2641b6b8820d
SFisGOD <jonathandumale@gmail.com>
2018-12-06 08:48:49 +0800

    Bench: 3057978

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -101,7 +101,7 @@ namespace {
 
   // MobilityBonus[PieceType-2][attacked] contains bonuses for middle and end game,
   // indexed by piece type and number of attacked squares in the mobility area.
-  constexpr Score MobilityBonus[][32] = {
+  Score MobilityBonus[][32] = {
     { S(-62,-81), S(-53,-56), S(-12,-30), S( -4,-14), S(  3,  8), S( 13, 15), // Knights
       S( 22, 23), S( 28, 27), S( 33, 33) },
     { S(-48,-59), S(-20,-23), S( 16, -3), S( 26, 13), S( 38, 24), S( 51, 42), // Bishops
@@ -117,6 +117,8 @@ namespace {
       S(106,184), S(109,191), S(113,206), S(116,212) }
   };
 
+TUNE(SetRange(-200, 300), MobilityBonus);
+  
   // Outpost[knight/bishop][supported by pawn] contains bonuses for minor
   // pieces if they occupy or can reach an outpost square, bigger if that
   // square is supported by a pawn.
@@ -125,6 +127,8 @@ namespace {
     { S( 9, 2), S(15, 5) }  // Bishop
   };
 
+  
+  
   // RookOnFile[semiopen/open] contains bonuses for each rook when there is
   // no (friendly) pawn on the rook file.
   constexpr Score RookOnFile[] = { S(18, 7), S(44, 20) };
