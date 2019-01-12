8f8ba00c3605a7785f282db543565fb58d02e75a
SFisGOD <jonathandumale@gmail.com>
2019-01-12 18:06:57 +0800

    Larger multiplier. Bench: 3508844

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -752,9 +752,9 @@ namespace {
                     + 12 * pos.count<PAWN>()
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
-                    + 16 * imb
+                    + 50 * imb
                     + 48 * !pos.non_pawn_material()
-                    -118 ;
+                    -100 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
