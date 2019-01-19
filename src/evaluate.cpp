af96fe29fd585d4120bf007b1b6890879e785b97
SFisGOD <jonathandumale@gmail.com>
2019-01-19 19:53:49 +0800

    Halfway values. Bench: 3902778

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -776,9 +776,9 @@ namespace {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material(WHITE) == BishopValueMg
             && pos.non_pawn_material(BLACK) == BishopValueMg)
-            sf = 8 + 4 * pe->pawn_asymmetry();
+            sf = 8 * pe->pawn_asymmetry() + 3 * pos.count<PAWN>(strongSide);
         else
-            sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
+            sf = std::min(45 + (pos.opposite_bishops() ? 2 : 12) * pos.count<PAWN>(strongSide), sf);
 
     }
 
