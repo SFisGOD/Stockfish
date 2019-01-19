53485cad11275f6758adcdf7fe072dad2ebf2943
SFisGOD <jonathandumale@gmail.com>
2019-01-19 19:38:55 +0800

    Extend QE. Bench: 3487949

--- src/search.cpp
+++ src/search.cpp
@@ -576,7 +576,7 @@ namespace {
     Value bestValue, value, ttValue, eval, maxValue, pureStaticEval;
     bool ttHit, pvHit, inCheck, givesCheck, improving;
     bool captureOrPromotion, doFullDepthSearch, moveCountPruning, skipQuiets, ttCapture;
-    Piece movedPiece;
+    Piece movedPiece, capturedPiece;
     int moveCount, captureCount, quietCount;
 
     // Step 1. Initialize node
@@ -925,6 +925,7 @@ moves_loop: // When in check, search starts from here
       extension = DEPTH_ZERO;
       captureOrPromotion = pos.capture_or_promotion(move);
       movedPiece = pos.moved_piece(move);
+      capturedPiece = pos.captured_piece();
       givesCheck = gives_check(pos, move);
 
       moveCountPruning =   depth < 16 * ONE_PLY
@@ -968,6 +969,12 @@ moves_loop: // When in check, search starts from here
 
       // Extension if castling
       else if (type_of(move) == CASTLING)
+          extension = ONE_PLY;
+	  
+      // Extension if queen exchange
+      if ( type_of(capturedPiece) == QUEEN 
+          && type_of(movedPiece) == QUEEN
+          && depth < 12 * ONE_PLY)
           extension = ONE_PLY;
 
       // Calculate new depth for this move
