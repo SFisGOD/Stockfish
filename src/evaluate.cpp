ed75fc8db611c42807370cbb896bf39f06e10201
SFisGOD <jonathandumale@gmail.com>
2019-01-19 15:09:12 +0800

    OCB sf. Bench: 3574500

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -769,6 +769,9 @@ namespace {
 
     Color strongSide = eg > VALUE_DRAW ? WHITE : BLACK;
     int sf = me->scale_factor(pos, strongSide);
+	
+    bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
+                            && (pos.pieces(PAWN) & KingSide);
 
     // If scale is not already specific, scale down the endgame via general heuristics
     if (sf == SCALE_FACTOR_NORMAL)
@@ -776,7 +779,7 @@ namespace {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material(WHITE) == BishopValueMg
             && pos.non_pawn_material(BLACK) == BishopValueMg)
-            sf = 8 + 4 * pe->pawn_asymmetry();
+            sf = 8 * pe->pawn_asymmetry() + 10 * pawnsOnBothFlanks ;
         else
             sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
 
