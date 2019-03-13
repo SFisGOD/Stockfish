16e0c350e2c3a42c200ffab3dc3d4478d59ce104
SFisGOD <jonathandumale@gmail.com>
2019-03-14 02:26:35 +0800

    Rook Endgames. Bench: 3694712

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -773,7 +773,10 @@ namespace {
     // If scale is not already specific, scale down the endgame via general heuristics
     if (sf == SCALE_FACTOR_NORMAL)
     {
-        if (   pos.opposite_bishops()
+        if (   pos.non_pawn_material(WHITE) == RookValueMg
+            && pos.non_pawn_material(BLACK) == RookValueMg)
+            sf = 32;
+        else if (   pos.opposite_bishops()
             && pos.non_pawn_material(WHITE) == BishopValueMg
             && pos.non_pawn_material(BLACK) == BishopValueMg)
             sf = 8 + 4 * pe->pawn_asymmetry();
