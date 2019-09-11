9dfb1ea34e700ace95694da635f96b51d8945e28
SFisGOD <jonathandumale@gmail.com>
2019-09-12 06:22:50 +0800

    Modify dct. Bench: 4166861

--- src/search.cpp
+++ src/search.cpp
@@ -418,7 +418,9 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + 86 * previousScore / (abs(previousScore) + 176);
+              int dct = (abs(previousScore) >  8 * PawnValueEg / 5 ? 
+                          ct/4 + 43 * previousScore / (abs(previousScore) + 176)
+                        : ct   + 86 * previousScore / (abs(previousScore) + 176));
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));
