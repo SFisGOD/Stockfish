895ba4c68ccba75d8a5ab4bcf664f11ba25f96a0
SFisGOD <jonathandumale@gmail.com>
2019-09-24 08:35:53 +0800

    More bonus. Bench: 4028782

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -123,7 +123,7 @@ namespace {
 
   // PassedRank[Rank] contains a bonus according to the rank of a passed pawn
   constexpr Score PassedRank[RANK_NB] = {
-    S(0, 0), S(10, 28), S(17, 33), S(15, 41), S(62, 72), S(168, 177), S(276, 260)
+    S(0, 0), S(15, 33), S(22, 38), S(22, 46), S(67, 77), S(173, 182), S(281, 265)
   };
 
   // Assorted bonuses and penalties
