2fc7dd1d35d8147a7c5a1de48577d5b773607490
SFisGOD <jonathandumale@gmail.com>
2019-10-03 14:23:14 +0800

    Neg ct. Bench: 3826307

--- src/search.cpp
+++ src/search.cpp
@@ -417,7 +417,8 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + 86 * previousScore / (abs(previousScore) + 176);
+              int mod_dct = (previousScore < -3 * PawnValueEg / 2 ? -ct : ct );
+              int dct = mod_dct + 86 * previousScore / (abs(previousScore) + 176);
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));
