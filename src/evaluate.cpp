e8eb08c33761d70543707fb79da180f001a187f1
SFisGOD <jonathandumale@gmail.com>
2019-10-07 21:15:57 +0800

    Tweak. Bench: 3735300

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -717,9 +717,9 @@ namespace {
                            && !pawnsOnBothFlanks;
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   9 * pe->passed_count()
-                    + 11 * pos.count<PAWN>()
-                    +  9 * outflanking
+    int complexity =  13 * pe->passed_count()
+                    +  9 * pos.count<PAWN>()
+                    +  4 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
