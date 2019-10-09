c9cc7471502342834683670fa15e227f74576c16
SFisGOD <jonathandumale@gmail.com>
2019-10-09 19:34:08 +0800

    Square. Bench: 4048826

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -719,7 +719,7 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
-                    +  9 * outflanking
+                    +  4 * outflanking * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
