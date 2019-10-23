5e78defb735dc3386509f80bb5c0f59c8abe9106
SFisGOD <jonathandumale@gmail.com>
2019-10-23 23:53:36 +0800

    OCB. Bench: 4343293

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -751,7 +751,7 @@ namespace {
     {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 16 + 4 * pe->passed_count();
+            sf = 4 * pe->passed_count() + + 4 * pos.count<PAWN>(strongSide);
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
