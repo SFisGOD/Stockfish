74b3109bed79af6867cfaf26f193c13962ef3e0c
SFisGOD <jonathandumale@gmail.com>
2019-10-25 18:44:27 +0800

    Less kd if no enemy bishop pair. Bench: 4847863

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -455,6 +455,7 @@ namespace {
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
                  - 873 * !pos.count<QUEEN>(Them)
+                 - 100 * bool(pos.count<BISHOP>(Them) != 2)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   3 * kingFlankAttacks * kingFlankAttacks / 8
