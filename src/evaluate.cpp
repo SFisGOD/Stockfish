12ce9ea5da89852326aa353e040f03c8a7cca574
SFisGOD <jonathandumale@gmail.com>
2019-10-26 18:13:41 +0800

    Take 1. Bench: 4627020

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,8 +457,8 @@ namespace {
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
-                 -   6 * mg_value(score) / 8
-                 -   7;
+                 -  35 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][ROOK] & attackedBy[Us][KING])
+                 -   6 * mg_value(score) / 8 ;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
