c941313111b9ad936acd5137129234b97e941659
SFisGOD <jonathandumale@gmail.com>
2019-11-04 01:33:01 +0800

    Take 2. Bench: 4434952

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -129,23 +129,23 @@ namespace {
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CorneredBishop     = S( 50, 50);
-  constexpr Score FlankAttacks       = S(  8,  0);
-  constexpr Score Hanging            = S( 69, 36);
-  constexpr Score KingProtector      = S(  7,  8);
-  constexpr Score KnightOnQueen      = S( 16, 12);
-  constexpr Score LongDiagonalBishop = S( 45,  0);
-  constexpr Score MinorBehindPawn    = S( 18,  3);
-  constexpr Score Outpost            = S( 32, 10);
-  constexpr Score PassedFile         = S( 11,  8);
-  constexpr Score PawnlessFlank      = S( 17, 95);
-  constexpr Score RestrictedPiece    = S(  7,  7);
-  constexpr Score RookOnQueenFile    = S(  7,  6);
-  constexpr Score SliderOnQueen      = S( 59, 18);
-  constexpr Score ThreatByKing       = S( 24, 89);
-  constexpr Score ThreatByPawnPush   = S( 48, 39);
-  constexpr Score ThreatBySafePawn   = S(173, 94);
-  constexpr Score TrappedRook        = S( 47,  4);
-  constexpr Score WeakQueen          = S( 49, 15);
+  constexpr Score FlankAttacks       = S(  7,  0);
+  constexpr Score Hanging            = S( 71, 37);
+  constexpr Score KingProtector      = S(  8,  8);
+  constexpr Score KnightOnQueen      = S( 16, 14);
+  constexpr Score LongDiagonalBishop = S( 43,  0);
+  constexpr Score MinorBehindPawn    = S( 18,  8);
+  constexpr Score Outpost            = S( 33, 13);
+  constexpr Score PassedFile         = S( 11,  7);
+  constexpr Score PawnlessFlank      = S( 16, 99);
+  constexpr Score RestrictedPiece    = S(  8,  7);
+  constexpr Score RookOnQueenFile    = S(  7,  5);
+  constexpr Score SliderOnQueen      = S( 61, 17);
+  constexpr Score ThreatByKing       = S( 26, 89);
+  constexpr Score ThreatByPawnPush   = S( 48, 40);
+  constexpr Score ThreatBySafePawn   = S(175, 96);
+  constexpr Score TrappedRook        = S( 50,  2);
+  constexpr Score WeakQueen          = S( 53, 13);
 
 #undef S
 
