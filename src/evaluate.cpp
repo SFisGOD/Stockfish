b7bae5d2855733f8a5001ac194f3c3e6894e1164
SFisGOD <jonathandumale@gmail.com>
2019-11-08 01:55:16 +0800

    Take 4. Bench: 4530379

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -450,8 +450,8 @@ namespace {
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
+                 -  45 * bool(attackedBy[Us][PAWN  ] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
-                 -  25 * bool(attackedBy[Us][PAWN  ] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   7;
 
