b73822ca6f2ec07aba886ca4afca031f953884a0
SFisGOD <jonathandumale@gmail.com>
2019-11-10 01:11:57 +0800

    Take 1b. Bench: 4212221

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -442,8 +442,8 @@ namespace {
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
-                 + 200 * !(pos.pieces(PAWN) & KingFlank[file_of(ksq)])
                  + 185 * popcount(kingRing[Us] & weak)
+                 + 150 * !(pos.pieces(PAWN) & KingFlank[file_of(ksq)])
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
