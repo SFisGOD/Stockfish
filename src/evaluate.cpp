f4aafdd7c87ab0eb21d6b28106567d12d107b9ef
SFisGOD <jonathandumale@gmail.com>
2019-11-10 17:15:25 +0800

    Take 2. Bench: 4520027

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -444,6 +444,7 @@ namespace {
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
+                 + 100 * !more_than_one(pos.pieces(Us, PAWN) & attackedBy[Us][KING])
                  +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttacks * kingFlankAttacks / 8
