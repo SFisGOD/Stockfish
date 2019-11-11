bffe72b0002a16bc4ba61693c9d60751fb68af78
SFisGOD <jonathandumale@gmail.com>
2019-11-11 17:33:39 +0800

    Take 1. Bench: 4685477

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -138,6 +138,7 @@ namespace {
   constexpr Score Outpost            = S( 32, 10);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
+  constexpr Score QueenBehindPawn    = S( 15,  5);
   constexpr Score RestrictedPiece    = S(  7,  7);
   constexpr Score RookOnQueenFile    = S(  7,  6);
   constexpr Score SliderOnQueen      = S( 59, 18);
@@ -358,6 +359,10 @@ namespace {
             Bitboard queenPinners;
             if (pos.slider_blockers(pos.pieces(Them, ROOK, BISHOP), s, queenPinners))
                 score -= WeakQueen;
+			
+            // Bonus for being right behind a pawn
+            if (shift<Down>(pos.pieces(PAWN)) & s)
+                score += QueenBehindPawn;
         }
     }
     if (T)
