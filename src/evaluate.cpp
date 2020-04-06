498467e4e9cac509e5c36de9d0f8876d09fbe5a3
SFisGOD <jonathandumale@gmail.com>
2020-04-06 15:55:16 +0800

    Try. Bench: 4916603

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -717,8 +717,8 @@ namespace {
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
-                    - 25 * rookEndgames
                     - 43 * almostUnwinnable
+                    - 50 * rookEndgames
                     -110 ;
 
     Value mg = mg_value(score);
