07104b484d743e965dff4f40ee887d072a1f7ba2
SFisGOD <jonathandumale@gmail.com>
2020-06-29 17:33:51 +0800

    Two rooks case. Bench: 4484371

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -781,13 +781,20 @@ namespace {
             else
                 sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
-        else if (  pos.non_pawn_material(WHITE) == RookValueMg
-                && pos.non_pawn_material(BLACK) == RookValueMg
-                && !pe->passed_pawns(strongSide)
+        else if (  !pe->passed_pawns(strongSide)
                 && pos.count<PAWN>(strongSide) - pos.count<PAWN>(~strongSide) <= 1
                 && bool(KingSide & pos.pieces(strongSide, PAWN)) != bool(QueenSide & pos.pieces(strongSide, PAWN))
                 && (attacks_bb<KING>(pos.square<KING>(~strongSide)) & pos.pieces(~strongSide, PAWN)))
+        {
+            if (   pos.non_pawn_material(WHITE) == RookValueMg
+                && pos.non_pawn_material(BLACK) == RookValueMg)
             sf = 36;
+            else if (   pos.non_pawn_material(WHITE) == RookValueMg * 2
+                     && pos.non_pawn_material(BLACK) == RookValueMg * 2)
+            sf = 43;
+            else
+            sf = std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide));
+        }
         else if (pos.count<QUEEN>() == 1)
             sf = 37;
         else
