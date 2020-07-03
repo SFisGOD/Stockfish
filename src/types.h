0ee8fcf1b7c8576aa72c4b8bb245c689db72fef0
SFisGOD <jonathandumale@gmail.com>
2020-07-03 15:17:21 +0800

    Try. Bench: 4590365

--- src/types.h
+++ src/types.h
@@ -180,8 +180,8 @@ enum Value : int {
   VALUE_MATE_IN_MAX_PLY  =  VALUE_MATE - MAX_PLY,
   VALUE_MATED_IN_MAX_PLY = -VALUE_MATE_IN_MAX_PLY,
 
-  PawnValueMg   = 124,   PawnValueEg   = 206,
-  KnightValueMg = 781,   KnightValueEg = 854,
+  PawnValueMg   = 120,   PawnValueEg   = 206,
+  KnightValueMg = 790,   KnightValueEg = 854,
   BishopValueMg = 825,   BishopValueEg = 915,
   RookValueMg   = 1276,  RookValueEg   = 1380,
   QueenValueMg  = 2538,  QueenValueEg  = 2682,
