ee11f2f2f5614780179c923587089de6c2e7470d
SFisGOD <jonathandumale@gmail.com>
2020-07-07 09:29:02 +0800

    Try. Bench: 4376041

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -786,8 +786,8 @@ namespace {
                 && !(pos.pieces(PAWN) & CenterFiles)
                 && !pawnsOnBothFlanks
                 && pos.count<PAWN>(~strongSide) - pos.count<PAWN>(strongSide) >= 0
-                && (pos.pieces(~strongSide, PAWN) & attackedBy[~strongSide][KING]))
-            sf = 20;
+                && pos.pawns_on_same_color_squares(~strongSide, pos.square<BISHOP>(strongSide)) >= 1)
+            sf = 36;
         else if (pos.count<QUEEN>() == 1)
             sf = 37 + 3 * (pos.count<QUEEN>(WHITE) == 1 ? pos.count<BISHOP>(BLACK) + pos.count<KNIGHT>(BLACK)
                                                         : pos.count<BISHOP>(WHITE) + pos.count<KNIGHT>(WHITE));
