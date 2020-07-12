d4d24e65615f5d68ba44458072d08891722d311c
SFisGOD <jonathandumale@gmail.com>
2020-07-12 15:28:26 +0800

    Try. Bench: 4859939

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -140,7 +140,7 @@ namespace {
   constexpr Score BishopXRayPawns     = S(  4,  5);
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
-  constexpr Score GoodReachableOutpost= S( 56, 22);
+  constexpr Score GoodReachableOutpost= S( 44, 22);
   constexpr Score Hanging             = S( 69, 36);
   constexpr Score KnightOnQueen       = S( 16, 11);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
