f4f673a901b2a61e2d5127932b334a14095c0dbb
SFisGOD <jonathandumale@gmail.com>
2020-07-15 09:52:50 +0800

    Try. Bench: 4666719

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -784,7 +784,7 @@ namespace {
         }
         else if (   pos.non_pawn_material(WHITE) == BishopValueMg
                  && pos.non_pawn_material(BLACK) == BishopValueMg)
-            sf = std::min(sf, 28 +  7 * (pos.count<PAWN>(strongSide) - pos.count<PAWN>(~strongSide))
+            sf = std::min(sf, 31 +  7 * (pos.count<PAWN>(strongSide) - pos.count<PAWN>(~strongSide))
                                  + 14 * pos.pawns_on_same_color_squares(~strongSide, pos.square<BISHOP>(strongSide)));
         else if (  pos.non_pawn_material(WHITE) == RookValueMg
                 && pos.non_pawn_material(BLACK) == RookValueMg
