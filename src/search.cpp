0fa671b82ec340c0ef9cbf29f21bf32b9642dc90
SFisGOD <jonathandumale@gmail.com>
2020-07-18 09:56:55 +0800

    Try. Bench: 4439316

--- src/search.cpp
+++ src/search.cpp
@@ -1129,7 +1129,7 @@ moves_loop: // When in check, search starts from here
 
       // Castling extension
       if (   type_of(move) == CASTLING
-          && popcount(pos.pieces(us) & (to_sq(move) & KingSide ? KingSide : QueenSide)) <= 3)
+          && popcount(pos.pieces(us) & (to_sq(move) & KingSide ? KingSide : QueenSide)) >= 4)
           extension = 1;
 
       // Late irreversible move extension
