fe5a3ea45258306c67981c072df618788a797cfe
SFisGOD <jonathandumale@gmail.com>
2020-07-26 15:16:31 +0800

    Try. Bench: 4358048

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -783,6 +783,9 @@ namespace {
             else
                 sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
+        else if (  pos.count<BISHOP>(WHITE) == 1
+                && pos.count<BISHOP>(BLACK) == 1)
+            sf = std::min(sf, 36 + 4 * pos.count<ALL_PIECES>(strongSide));
         else if (  pos.non_pawn_material(WHITE) == RookValueMg
                 && pos.non_pawn_material(BLACK) == RookValueMg
                 && pos.count<PAWN>(strongSide) - pos.count<PAWN>(~strongSide) <= 1
