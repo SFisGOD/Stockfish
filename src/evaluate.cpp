808f868b788fc4d38afaa7d2efa1e6185a6ba2cb
SFisGOD <jonathandumale@gmail.com>
2020-08-08 19:36:17 +0800

    Try. Bench: 4067325

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -942,7 +942,7 @@ Value Eval::evaluate(const Position& pos) {
       Value v = eg_value(pos.psq_score());
       // Take NNUE eval only on balanced positions
       if (abs(v) < NNUEThreshold)
-         return NNUE::evaluate(pos) + Tempo;
+         return (5 * NNUE::evaluate(pos) / 6) + (Evaluation<NO_TRACE>(pos).value() / 6) + Tempo;
   }
   return Evaluation<NO_TRACE>(pos).value();
 }
