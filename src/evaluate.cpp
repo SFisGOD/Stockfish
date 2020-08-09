9c7de429793bf4ec5442203b9b95380ed65d47ff
SFisGOD <jonathandumale@gmail.com>
2020-08-09 13:31:00 +0800

    Try. Bench: 3882526

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -181,6 +181,7 @@ namespace {
   constexpr Score MinorBehindPawn     = S( 18,  3);
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);
+  constexpr Score QueenInfiltration   = S(  0, 16);
   constexpr Score ReachableOutpost    = S( 31, 22);
   constexpr Score RestrictedPiece     = S(  7,  7);
   constexpr Score RookOnKingRing      = S( 16,  0);
@@ -423,6 +424,10 @@ namespace {
             Bitboard queenPinners;
             if (pos.slider_blockers(pos.pieces(Them, ROOK, BISHOP), s, queenPinners))
                 score -= WeakQueen;
+			
+            // Bonus for queen on weak square in enemy camp
+            if (relative_rank(Us, s) > RANK_5 && (~pe->pawn_attacks_span(Them) & s))
+                score += QueenInfiltration;
         }
     }
     if (T)
