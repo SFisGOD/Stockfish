e55bc13469d2dfde492018abb7b46dc659653958
SFisGOD <jonathandumale@gmail.com>
2020-08-12 06:50:18 +0800

    Try. Bench: 3862621

--- src/search.cpp
+++ src/search.cpp
@@ -1145,6 +1145,7 @@ moves_loop: // When in check, search starts from here
       // Late irreversible move extension
       if (   move == ttMove
           && pos.rule50_count() > 80
+          && pos.count<PAWN>() >= 12
           && (captureOrPromotion || type_of(movedPiece) == PAWN))
           extension = 2;
 
