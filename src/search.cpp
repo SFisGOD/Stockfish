802cd353fdce5bb04ed9b66041416f5ad5a365fa
SFisGOD <jonathandumale@gmail.com>
2020-08-14 15:37:19 +0800

    Remove. Bench: 3988048

--- src/search.cpp
+++ src/search.cpp
@@ -1145,7 +1145,7 @@ moves_loop: // When in check, search starts from here
       // Late irreversible move extension
       if (   move == ttMove
           && pos.rule50_count() > 80
-          && (captureOrPromotion || type_of(movedPiece) == PAWN))
+          && type_of(movedPiece) == PAWN)
           extension = 2;
 
       // Add extension to new depth
