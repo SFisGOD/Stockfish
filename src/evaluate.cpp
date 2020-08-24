477f0e380039157e5ab21c084dae025df8ac82d0
SFisGOD <jonathandumale@gmail.com>
2020-08-25 06:38:01 +0800

    Try. Bench: 3463724

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -940,10 +940,17 @@ make_v:
 
 Value Eval::evaluate(const Position& pos) {
 
+  bool majors =   pos.count<KNIGHT>() == 0
+               && pos.count<BISHOP>() == 0
+               && pos.count<QUEEN>() == 2;
+			   
   bool classical = !Eval::useNNUE
                 ||  abs(eg_value(pos.psq_score())) * 16 > NNUEThreshold1 * (16 + pos.rule50_count());
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
+					  
+  if (!classical && majors)
+      v = v * 3 / 5;
 
   if (classical && Eval::useNNUE && abs(v) * 16 < NNUEThreshold2 * (16 + pos.rule50_count()))
       v = NNUE::evaluate(pos) * 5 / 4 + Tempo;
