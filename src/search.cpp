3f44a1af9cefefaec34379328594617262e57c7e
SFisGOD <jonathandumale@gmail.com>
2020-08-30 16:35:16 +0800

    Try. Bench: 4031185

--- src/search.cpp
+++ src/search.cpp
@@ -1120,6 +1120,7 @@ moves_loop: // When in check, search starts from here
 		  
       // Passed pawn extension
       if (   abs(eg_value(pos.psq_score())) * 16 > Value(550) * (16 + pos.rule50_count())
+          && move == ss->killers[0]
           && pos.advanced_pawn_push(move)
           && pos.pawn_passed(us, to_sq(move)))
           extension = 1;
