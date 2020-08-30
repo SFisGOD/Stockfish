c22fa103a705c58086b19faab0b38dd76a620dc6
SFisGOD <jonathandumale@gmail.com>
2020-08-30 16:14:53 +0800

    Try. Bench: 3712169

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -947,6 +947,10 @@ Value Eval::evaluate(const Position& pos) {
 
   if (classical && Eval::useNNUE && abs(v) * 16 < NNUEThreshold2 * (16 + pos.rule50_count()))
       v = NNUE::evaluate(pos) * 5 / 4 + Tempo;
+  
+  if (!classical && abs(eg_value(pos.psq_score())) >= PawnValueEg)
+      v += (pos.side_to_move() == WHITE ?  pos.count<PAWN>(WHITE)
+                                        : -pos.count<PAWN>(BLACK));
 
   // Damp down the evaluation linearly when shuffling
   v = v * (100 - pos.rule50_count()) / 100;
