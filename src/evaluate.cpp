522152a833308bcd191e69d7389b5e33a2de832a
SFisGOD <jonathandumale@gmail.com>
2020-09-09 04:06:52 +0800

    Try. Bench: 3974268

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1021,7 +1021,9 @@ Value Eval::evaluate(const Position& pos) {
   bool useClassical = abs(eg_value(pos.psq_score())) * 16 > NNUEThreshold1 * (16 + pos.rule50_count());
   bool classical = !Eval::useNNUE
                 ||  useClassical
-                || (abs(eg_value(pos.psq_score())) > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
+                || (abs(eg_value(pos.psq_score())) > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB))
+                || (abs(eg_value(pos.psq_score())) > PawnValueMg / 8 && !(pos.this_thread()->nodes & 0xF))
+                || !(pos.this_thread()->nodes & 0x1F);
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
 
