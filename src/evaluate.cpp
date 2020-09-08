6e6fc5cf6a030d2fe11cca10cacc01f75da62f24
SFisGOD <jonathandumale@gmail.com>
2020-09-09 04:31:07 +0800

    Try. Bench: 3974268

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1018,12 +1018,14 @@ Value Eval::evaluate(const Position& pos) {
   // Use classical eval if there is a large imbalance
   // If there is a moderate imbalance, use classical eval with probability (1/8),
   // as derived from the node counter.
-  bool useClassical = abs(eg_value(pos.psq_score())) * 16 > NNUEThreshold1 * (16 + pos.rule50_count());
+  int psqt = abs(eg_value(pos.psq_score()));
+  int node = pos.this_thread()->nodes;
+  bool useClassical = psqt * 16 > NNUEThreshold1 * (16 + pos.rule50_count());
   bool classical = !Eval::useNNUE
                 ||  useClassical
-                || (abs(eg_value(pos.psq_score())) > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB))
-                || (abs(eg_value(pos.psq_score())) > PawnValueMg / 8 && !(pos.this_thread()->nodes & 0xF))
-                || !(pos.this_thread()->nodes & 0x1F);
+                || (psqt > PawnValueMg / 4 && !(node & 0xB))
+                || (!(psqt > PawnValueMg / 4) && psqt > PawnValueMg / 8 && !(node & 0xF))
+                || (!(psqt > PawnValueMg / 4) && !(psqt > PawnValueMg / 8) && !(node & 0x1F));
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
 
