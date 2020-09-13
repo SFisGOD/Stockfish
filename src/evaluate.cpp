98fff61b90cd59f9d4e446666dfe0ca629170601
SFisGOD <jonathandumale@gmail.com>
2020-09-13 10:12:04 +0800

    Try. Bench: 4325138

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1016,15 +1016,23 @@ make_v:
 Value Eval::evaluate(const Position& pos) {
 
   // Use classical eval if there is a large imbalance
-  // If there is a moderate imbalance, use classical eval with probability (1/8),
+  // For moderate imbalance, use classical eval with probability (1/8),
   // as derived from the node counter.
-  bool useClassical = abs(eg_value(pos.psq_score())) * 16 > NNUEThreshold1 * (16 + pos.rule50_count());
+  Value psqt = Value(abs(eg_value(pos.psq_score())));
+  bool useClassical = psqt * 16 > NNUEThreshold1 * (16 + pos.rule50_count());
   bool classical = !Eval::useNNUE
                 ||  useClassical
-                || (abs(eg_value(pos.psq_score())) > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
+                || (psqt > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
 
+  // Use classical eval if NNUE eval is low
+  if (   !classical 
+      && abs(v) < PawnValueMg / 4
+      && psqt > 2 * PawnValueMg)
+      v = Evaluation<NO_TRACE>(pos).value();
+
+  // Fall back to NNUE if classical eval is smaller than expected
   if (   useClassical 
       && Eval::useNNUE 
       && abs(v) * 16 < NNUEThreshold2 * (16 + pos.rule50_count()))
