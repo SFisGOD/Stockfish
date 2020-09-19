e70c5689d93e25da7c1d969b6411cd97a9a3bfbb
SFisGOD <jonathandumale@gmail.com>
2020-09-19 18:00:58 +0800

    Try. Bench: 4061992

--- src/search.cpp
+++ src/search.cpp
@@ -87,7 +87,7 @@ namespace {
 
   // Add a small random component to draw evaluations to avoid 3fold-blindness
   Value value_draw(Score psq, Thread* thisThread) {
-    return VALUE_DRAW + Value((abs(eg_value(psq)) > PawnValueMg / 16 ? 3 : 2) * (thisThread->nodes & 1) - 1);
+    return VALUE_DRAW + Value((abs(eg_value(psq)) > PawnValueMg / 8 ? 4 : 3) * (thisThread->nodes & 1) - 1);
   }
 
   // Skill structure is used to implement strength limit
