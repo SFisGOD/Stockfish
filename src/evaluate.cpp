8917d05235c272f63b6149956e36022569460734
SFisGOD <jonathandumale@gmail.com>
2020-09-27 02:58:41 +0800

    Try. Bench: 3997077

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -896,9 +896,9 @@ namespace {
         {
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = 18 + 4 * popcount(pe->passed_pawns(strongSide));
+                sf = 10 + 4 * popcount(pe->passed_pawns(strongSide));
             else
-                sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);
+                sf = 14 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
         else if (  pos.non_pawn_material(WHITE) == RookValueMg
                 && pos.non_pawn_material(BLACK) == RookValueMg
