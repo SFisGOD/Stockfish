c329eae995375a9833b1f648a94b354d8e5f40ae
SFisGOD <jonathandumale@gmail.com>
2020-10-08 07:48:26 +0800

    Try. Bench: 3948734

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1035,6 +1035,12 @@ Value Eval::evaluate(const Position& pos) {
 
       v = classical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
 
+      if (   !(pos.this_thread()->nodes & 0x7)
+          && (pos.count<BISHOP>(WHITE) == 1 && pos.count<BISHOP>(BLACK) == 1)
+          && abs(v) * 16 < NNUEThreshold2 * r50
+          && !classical)
+          v = Evaluation<NO_TRACE>(pos).value();
+
       // If the classical eval is small and imbalance large, use NNUE nevertheless.
       // For the case of opposite colored bishops, switch to NNUE eval with
       // small probability if the classical eval is less than the threshold.
