e542a2bcbee612f569f2734233a037b827a87df8
SFisGOD <jonathandumale@gmail.com>
2020-10-24 04:49:03 +0800

    Bench: 3518680

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1035,12 +1035,15 @@ Value Eval::evaluate(const Position& pos) {
       // If the classical eval is small and imbalance large, use NNUE nevertheless.
       // For the case of opposite colored bishops, switch to NNUE eval with
       // small probability if the classical eval is less than the threshold.
-      if (   largePsq
-          && (abs(v) * 16 < NNUEThreshold2 * r50
-          || (   pos.opposite_bishops()
-              && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
-              && !(pos.this_thread()->nodes & 0xB))))
-          v = adjusted_NNUE();
+      if (largePsq)
+      {
+          if (abs(v) * 16 < NNUEThreshold2 * r50)
+              v = adjusted_NNUE();
+          else if (   !(pos.this_thread()->nodes & 0xB)
+                   && (pos.opposite_bishops() || !((pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide)))
+                   && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50)
+              v = adjusted_NNUE();
+      }
   }
 
   // Damp down the evaluation linearly when shuffling
