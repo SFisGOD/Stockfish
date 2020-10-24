63ff8ab2356cc59f38636fbd5594531fd3b2ffe1
SFisGOD <jonathandumale@gmail.com>
2020-10-24 13:42:25 +0800

    Bench: 3990695

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1030,7 +1030,14 @@ Value Eval::evaluate(const Position& pos) {
       bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
       bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
 
-      v = classical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
+      if (!classical)
+          v = adjusted_NNUE();
+      else if (   classical
+               && psq < QueenValueMg
+               && popcount(pos.pieces(WHITE, PAWN) & shift<-pawn_push(WHITE)>(pos.pieces(BLACK, PAWN))) >= 6)
+          v = adjusted_NNUE();
+      else
+          v = Evaluation<NO_TRACE>(pos).value();
 
       // If the classical eval is small and imbalance large, use NNUE nevertheless.
       // For the case of opposite colored bishops, switch to NNUE eval with
