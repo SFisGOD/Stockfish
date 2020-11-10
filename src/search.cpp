df53c4184839ae46ec5bf0df1bcbdafa16bdaafc
SFisGOD <jonathandumale@gmail.com>
2020-11-10 20:56:54 +0800

    Bench: 3787506

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,10 @@ moves_loop: // When in check, search starts from here
               r -= 2;
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
-          if (!(PvNode && !rootNode) && depth > 10 && thisThread->bestMoveChanges <= 2)
+          // For PV nodes, increase reduction with 1 in 16 probability
+          if (   (!(PvNode && !rootNode) || ((PvNode && !rootNode) && !(thisThread->nodes & 0xF)))
+              && depth > 10
+              && thisThread->bestMoveChanges <= 2)
               r++;
 
           if (moveCountPruning && !formerPv)
