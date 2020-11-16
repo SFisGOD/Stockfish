2bb8a2cf2c962943a02a9267c76ddce07c5f4f1f
SFisGOD <jonathandumale@gmail.com>
2020-11-16 11:01:53 +0800

    Bench: 3614752

--- src/search.cpp
+++ src/search.cpp
@@ -1173,6 +1173,10 @@ moves_loop: // When in check, search starts from here
           if ((rootNode || !PvNode) && depth > 10 && thisThread->bestMoveChanges <= 2)
               r++;
 
+          // Increase reduction for king moves at non-PV nodes
+          if (!PvNode && type_of(pos.moved_piece(move)) == KING)
+              r++;
+
           if (moveCountPruning && !formerPv)
               r++;
 
