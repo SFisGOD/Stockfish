16ff79e051ea1ee678eebc664db274f4559496c6
SFisGOD <jonathandumale@gmail.com>
2020-11-22 03:21:04 +0800

    Tune

--- src/search.cpp
+++ src/search.cpp
@@ -61,7 +61,7 @@ namespace {
   TUNE(netbiases);
   int netweights[32] = {-24, -16, -75, 55, -17, 122, -118, 22, 32, 50, -34, 19, 
   15, -37, -20, 97, -54, 30, 35, 41, -18, -20, 17, -30, -12, -37, -21, -10, -29, 28, -13, 17};
-  auto myfunc127 = [](int m){ return std::pair<int, int>(std::max(-127, m - 40),std::min(127,m + 40));};
+  auto myfunc127 = [](int m){ return std::pair<int, int>(std::max(-127, m - 80),std::min(127,m + 80));};
   TUNE(SetRange(myfunc127), netweights);
 
   // Different node types, used as a template parameter
